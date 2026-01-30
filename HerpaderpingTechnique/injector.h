1634106d415c28bb3d59266f46ef2b79fb8d25f5
0xufo <bassamhossam54@gmail.com>
2026-01-30 11:06:43 +0200

    Update injector.h

--- HerpaderpingTechnique/injector.h
+++ HerpaderpingTechnique/injector.h
@@ -2,8 +2,6 @@
 #include <Windows.h>
 #include "globals.h"
 
-// Renamed from InitializeProcessParms
 BOOL SetupProcessParameters(IN HANDLE processHandle, IN LPWSTR targetProcessPath, OUT PVOID* outImageBase);
 
-// Renamed from HerpaderpProcess
 BOOL ExecuteHerpaderping(IN LPWSTR tempFilePath, IN LPWSTR legitImagePath, IN PBYTE payloadBuffer, IN DWORD payloadSize);
